cde36c6516fbe91ed81b2465e352b8e9155b95cb
mcr45 <depr3604@protonmail.com>
2024-02-21 22:08:52 -0600

    create auth with jwt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :authenticate_request
     def create
         post=Post.new(post_params)
         if post.save
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,17 @@ class SessionsController < ApplicationController
   def create
     user=User.find_by(username:params[:username])
     if user&.authenticate(params[:password])
-      render json: {token:'token'},status: :ok
+      token=jwt_encode(user_id:user.id)
+      render json: {token:token},status: :ok
     else
       render json: {error:'invalid credentials'}, status: :unauthorized
     end
+  end
+
+
+
+  def jwt_encode(payload,exp=24.hours.from_now)
+    payload[:exp]=exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 end
