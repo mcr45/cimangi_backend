19a4be10ccd48b1088fc6689a73b29a5dae18de0
mcr45 <depr3604@protonmail.com>
2024-04-18 00:31:21 -0500

    giving user more charachters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :liked_by, class_name:"Like"
   validates :title,:body, presence: true
-  validates :body,length: {in:20..256}
+  validates :body,length: {in:20..9956}
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, as: :commentable
   #validates :user_id,optional:true
-  validates :body, presence:true,length:{minimum:15,maximum:300}
-  validates :title, presence:true, length:{minimum:10,maximum:25}
+  validates :body, presence:true,length:{minimum:15,maximum:9000}
+  validates :title, presence:true, length:{minimum:10,maximum:105}
   validates :category, presence:true,length:{minimum:3,maximum:20}
 end
