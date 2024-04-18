49e479c6e2dafe69be7b2d63d280191ebc1670a2
mcr45 <depr3604@protonmail.com>
2024-04-17 22:13:32 -0500

    not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_request
+    before_action :authenticate_request,only:[:load_user]
 def create
     user=User.new(user_params)
     if user.save
